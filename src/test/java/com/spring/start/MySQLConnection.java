3f126cd4a46c351f3c4ccd6922e9a7aa59a00cce
kang <abcnt@naver.com>
2018-07-25 11:50:28 +0900

    jdbc 연동

--- src/test/java/com/spring/start/MySQLConnection.java
+++ src/test/java/com/spring/start/MySQLConnection.java
@@ -6,12 +6,16 @@ import java.sql.DriverManager;
 import org.junit.Test;
 
 public class MySQLConnection {
-String connectionString = "jdbc:mysql://localhost:3306/spring_prj?user=root&password=eorn1145&useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC";
-	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-	private static final String URL = "jdbc:mysql://192.168.0.13:3308/spring_prj";
-	private static final String USER = "root";
-	private static final String PWD = "eorn1145";
+
 	
+	String connectionString = "jdbc:mysql://localhost:3306/spring_prj?user=root&password=eorn1145&useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC";
+
+	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+//	private static final String URL = "jdbc:mysql://192.168.0.13:3308/spring_prj";
+//	private static final String USER = "root";
+//	private static final String PWD = "eorn1145";
+	
+	//junit 테스트
 	@Test
 	public void testConn() throws Exception{
 		
@@ -20,7 +24,7 @@ String connectionString = "jdbc:mysql://localhost:3306/spring_prj?user=root&pass
 		try(Connection conn = DriverManager.getConnection(connectionString)){
 			
 			System.out.println(conn);
-			
+			//com.mysql.cj.jdbc.ConnectionImpl@4bb4de6a
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
