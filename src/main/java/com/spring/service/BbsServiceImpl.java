7f637a897ed27ed2853c04d1a757656b0acb1b5a
kang <abcnt@naver.com>
2018-07-26 17:18:16 +0900

    BbsService를 상속한 BbsServiceImpl 클래스 구현

---
+++ src/main/java/com/spring/service/BbsServiceImpl.java
@@ -0,0 +1,45 @@
+package com.spring.service;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import com.spring.dao.BbsDAO;
+import com.spring.vo.BbsVO;
+
+public class BbsServiceImpl implements BbsService {
+
+	@Inject
+	private BbsDAO bdao;
+	
+	@Override
+	public void write(BbsVO bvo) throws Exception {
+		bdao.insert(bvo);
+
+	}
+
+	@Override
+	public BbsVO read(Integer bid) throws Exception {
+		
+		return bdao.read(bid);
+	}
+
+	@Override
+	public void modify(BbsVO bvo) throws Exception {
+		
+		bdao.update(bvo);
+	}
+
+	@Override
+	public void remove(Integer bid) throws Exception {
+		bdao.delete(bid);
+
+	}
+
+	@Override
+	public List<BbsVO> list() throws Exception {
+		
+		return bdao.list();
+	}
+
+}
