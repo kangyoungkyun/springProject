27b5ba010895acc8ae1ff5237e9c8affbfa030a9
kang <abcnt@naver.com>
2018-07-26 14:25:37 +0900

    BbsDAO를 상속한 BbsDAOImpl  클래스 작성

---
+++ src/main/java/com/spring/dao/BbsDAOImpl.java
@@ -0,0 +1,47 @@
+package com.spring.dao;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.spring.vo.BbsVO;
+
+public class BbsDAOImpl implements BbsDAO{
+
+	
+	@Inject
+	private SqlSession sqlSession;
+	
+	
+	@Override
+	public void insert(BbsVO bvo) throws Exception {
+		sqlSession.insert("insert",bvo);
+		
+	}
+
+	@Override
+	public BbsVO read(Integer bid) throws Exception {
+		
+		return sqlSession.selectOne("read",bid);
+	}
+
+	@Override
+	public void update(BbsVO bvo) throws Exception {
+		sqlSession.update("update",bvo);
+		
+	}
+
+	@Override
+	public void delete(Integer bid) throws Exception {
+		sqlSession.delete("delete",bid);
+		
+	}
+
+	@Override
+	public List<BbsVO> list() throws Exception {
+		return sqlSession.selectList("list");
+	}
+
+}
